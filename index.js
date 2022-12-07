28becb8f958ca2af7322322f1e08d1e8a979855b
unknown <straxpubgm88888888ey>
2022-12-07 16:22:08 +0300

    ok

--- index.js
+++ index.js
@@ -38,3 +38,40 @@ function onGooglePayLoaded(){
         })
         .catch(error=>console.error('ISREADYTOPAY',error))
 }
+
+function createAndAddButton(){
+    const googlePayButton = googlePayClient.createButton({
+        onClick: onGooglePayButtonClicked,
+    });
+    document.getElementById('buy-now').appendChild(googlePayButton)
+}
+
+function onGooglePayButtonClicked(){
+    const paymentDataRequest = {...googlePayConfiguration}
+    paymentDataRequest.merchantInfo = {
+        merchantId: "BCR2DN4T4SYZB7C3",
+        merchantName: "STRAX_SHOP"
+    }
+
+    paymentDataRequest.transactioninfo = {
+        totalPriceStatus: 'FINAL',
+        totalPriceLabel: 'Total',
+        totalPrice: '100.00',
+        currencyCode: 'USD',
+        countryCode: 'US',
+    }
+
+    googlePayClient.loadPaymentData(paymentDataRequest)
+        .then(paymentData=> processPaymentData(paymentData))
+        .catch(error=>console.error("LOAD PAYMENT DATA ", error))
+}
+
+function processPaymentData(paymentData){
+    fetch(ordersEndpointUrl,{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:paymentData
+    })
+}
